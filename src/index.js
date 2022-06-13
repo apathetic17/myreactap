f39ccb50cc562b98e32c82574dc0195958873b41
unknown <birkinkostea@gmail.com>
2022-06-13 18:04:49 +0300

    wafles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Carts from'./pages/Carts';
+import Products from './pages/Products';
+import {
+    BrowserRouter, Router,
+    Route
+  } from "react-router-dom";
+
+
+function ServerInstall(){
+    const [cart, setCart] = useState()
+    return (
+        <BrowserRouter>
+        <Router>
+            <Route path="/" element={<App />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/carts" element={<Carts />} />
+            <Route path="*" element={<App />} />
+        </Router>
+        </BrowserRouter>
+    )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+    <ServerInstall />
   
 );
 
